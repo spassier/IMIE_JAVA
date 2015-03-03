2a25805444fab31946210bbd6ce7a6fc1f32d9ad
Sebastien PASSIER <spassier@free.fr>
2015-03-03 10:36:24 +0100

    TP JUnit - Add tests using mock

--- Tennis/Tests/TennisTest.java
+++ Tennis/Tests/TennisTest.java
@@ -12,15 +12,15 @@ public class TennisTest {
 
 	private ISerialiser serialiser; 
 	private Jeux jeux;
-	private Joueur joueur1;
-	private Joueur joueur2;
+	//private Joueur joueur1;
+	//private Joueur joueur2;
 	
 	@Before
 	public void testInitialisationNouveauTest() {
 		serialiser = new Serialiser();
 		jeux = new Jeux(serialiser);
-		joueur1 = new Joueur();
-		joueur2 = new Joueur();
+		//joueur1 = new Joueur();
+		//joueur2 = new Joueur();
 	}
 	
 	@Test
@@ -30,100 +30,120 @@ public class TennisTest {
 
 	@Test
 	public void testAfficherScore_15_0() {
-		jeux.joueur1MarqueUnPoint(joueur1);
+		jeux.joueur1MarqueUnPoint();
 		Assert.assertEquals("15-0", jeux.getScore() );
 	}
 	
 	@Test
 	public void testAfficherScore_30_0() {	
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur1MarqueUnPoint(joueur1);
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
 		Assert.assertEquals("30-0", jeux.getScore() );
 	}
 	
 	@Test
 	public void testAfficherScore_40_0() {	
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur1MarqueUnPoint(joueur1);
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
 		Assert.assertEquals("40-0", jeux.getScore() );
 	}
 	
 	@Test
-	public void testAfficherScore_Victoire() {	
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur1MarqueUnPoint(joueur1);
-		Assert.assertEquals("Victoire", jeux.getScore() );
+	public void testAfficherScore_VictoireJ1() {	
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		Assert.assertEquals("Victoire joueur1", jeux.getScore() );
 	}
 	
 	@Test
 	public void testAfficherScore_0_15() {
-		jeux.joueur2MarqueUnPoint(joueur2);
+		jeux.joueur2MarqueUnPoint();
 		Assert.assertEquals("0-15", jeux.getScore() );
 	}
 	
 	@Test
 	public void testAfficherScore_0_30() {	
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
 		Assert.assertEquals("0-30", jeux.getScore() );
 	}
 	
 	@Test
 	public void testAfficherScore_0_40() {	
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
 		Assert.assertEquals("0-40", jeux.getScore() );
 	}
 	
 	@Test
-	public void testJoueurGagnePartie() {
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
-		Assert.assertEquals("Victoire", jeux.getScore() );
-	}
-	
-	@Test
 	public void testAfficherScoreEgalite() {
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.comparePointsJoueurs(joueur1, joueur2);
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		//jeux.comparePointsJoueurs(joueur1, joueur2);
 		Assert.assertEquals("Egalite", jeux.getScore() );
 	}
 	
 	@Test
 	public void testAfficherAvantageJoueur1() {
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur1MarqueUnPoint(joueur1);
-		jeux.comparePointsJoueurs(joueur1, joueur2);
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		//jeux.comparePointsJoueurs(joueur1, joueur2);
 		Assert.assertEquals("Avantage joueur1", jeux.getScore() );
 	}
 	
 	@Test
 	public void testAfficherAvantageJoueur2() {
-		jeux.joueur1MarqueUnPoint(joueur2);
-		jeux.joueur1MarqueUnPoint(joueur2);
-		jeux.joueur1MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur1);
-		jeux.joueur2MarqueUnPoint(joueur1);
-		jeux.joueur2MarqueUnPoint(joueur1);
-		jeux.joueur1MarqueUnPoint(joueur2);
-		jeux.comparePointsJoueurs(joueur1, joueur2);
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		//jeux.comparePointsJoueurs(joueur1, joueur2);
 		Assert.assertEquals("Avantage joueur2", jeux.getScore() );
+	}
+	
+	@Test
+	public void testAfficherAvantageJoueurs2PuisEgalite() {
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		//jeux.comparePointsJoueurs(joueur1, joueur2);
+		Assert.assertEquals("Egalite", jeux.getScore() );
+	}
+	
+	@Test
+	public void testAfficherAvantageJoueurs2PuisEgalitePuisAvantageJoueur1() {
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		//jeux.comparePointsJoueurs(joueur1, joueur2);
+		Assert.assertEquals("Avantage joueur1", jeux.getScore() );
 	}
 	
 	@Test
@@ -134,18 +154,20 @@ public class TennisTest {
 	}
 	
 	@Test
-	public void testPersistScore15_0() {
-		jeux.joueur1MarqueUnPoint(joueur1);
+	public void testPersistScore15_15() {
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
 		jeux.persist(jeux);
-		Assert.assertEquals("15-0", jeux.read().getScore() );
+		Assert.assertEquals("15-15", jeux.read().getScore() );
 	}
 	
 	@Test
-	public void testPersistScore0_40() {
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
-		jeux.joueur2MarqueUnPoint(joueur2);
+	public void testPersistScore30_30() {
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur1MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
+		jeux.joueur2MarqueUnPoint();
 		jeux.persist(jeux);
-		Assert.assertEquals("0-40", jeux.read().getScore() );
+		Assert.assertEquals("30-30", jeux.read().getScore() );
 	}
 }
--- Tennis/src/fr/imie/tennis/Jeux.java
+++ Tennis/src/fr/imie/tennis/Jeux.java
@@ -4,38 +4,101 @@ public class Jeux implements ISerialiser {
 	
 	private ISerialiser jeuSerialiser;
 	private String score = "0-0";
-		
+	private Integer j1 = 0;
+	private Integer j2 = 0;
+	
 	public Jeux (ISerialiser serialiser) {
 		jeuSerialiser = serialiser;
 	}
+	/*
+	0 = 0
+	1 = 15
+	2 = 30
+	3 = 40
+	4 = egalite ou victoire
+	 */
 	
 	public String getScore() {
+		
+
+		if ( (j1 < 4) && (j2 < 4) && !( j1 == 3 && j2 == 3 ) ) {
+			score = convertionPointsEnString(j1) + "-" + convertionPointsEnString(j2);
+		}
+		else if ( ( j1 >= 3 || j2 >= 3 ) ) {
+			if ( j1 == j2 )
+				score = "Egalite";
+			else if ( j1 - j2 == 1 )
+				score = "Avantage joueur1";
+			else if ( j2 - j1 == 1 )
+				score = "Avantage joueur2";
+			else if ( j1 - j2 > 1)
+				score = "Victoire joueur1";
+			else if ( j2 - j1 > 1)
+				score = "Victoire joueur2";
+		}
 		return score;
 	}
 	
-	public void joueur1MarqueUnPoint(Joueur joueur) {
+	private String convertionPointsEnString(Integer points) {	
+		String score = null;
+		
+		switch ( points )
+		{
+		case 0:
+			score = "0";
+			break;
+		case 1:
+			score = "15";
+			break;
+		case 2:
+			score = "30";
+			break;
+		case 3:
+			score = "40";
+			break;
+		/*
+		case 4:
+			score = "Victoire";
+			break;
+		*/
+		default :
+			throw new IllegalStateException(); 
+		}
+		
+		return score;
+	}
+	public void joueur1MarqueUnPoint(/*Joueur joueur*/) {
+		j1++;
+		/*
 		joueur.marqueUnPoint();
 		if ( joueur.getPoints() == 4)
 			score = joueur.getPointsConvertionEnScore();
 		else
 			score = joueur.getPointsConvertionEnScore() + score.substring(score.indexOf("-"));
+			*/
 	}
 	
-	public void joueur2MarqueUnPoint(Joueur joueur) {
+	public void joueur2MarqueUnPoint(/*Joueur joueur*/) {
+		j2++;
+		/*
 		joueur.marqueUnPoint();
 		if ( joueur.getPoints() == 4)
 			score = joueur.getPointsConvertionEnScore();
 		else
 			score = score.substring(0, score.indexOf("-")) + "-" +  joueur.getPointsConvertionEnScore();
+			*/
 	}
 	
 	public void comparePointsJoueurs(Joueur joueur1, Joueur joueur2){
-		if ( ( joueur1.getPoints() >= 3 ) && ( joueur1.getPoints() == joueur2.getPoints() ) )
-			score = "Egalite";
-		else if ( joueur1.getPoints() == 4 && joueur2.getPoints() == 3 )
-			score = "Avantage joueur1";
-		else if ( joueur1.getPoints() == 3 && joueur2.getPoints() == 4 )
-			score = "Avantage joueur2";
+		if ( ( joueur1.getPoints() >= 3 || joueur2.getPoints() >= 3 ) ) {
+			
+			if (  joueur1.getPoints() == joueur2.getPoints()  )
+				score = "Egalite";
+			else if ( joueur1.getPoints() - joueur2.getPoints() == 1 )
+				score = "Avantage joueur1";
+			else if ( joueur2.getPoints() - joueur1.getPoints() == 1 )
+				score = "Avantage joueur2";
+		}
 	}
 
 	@Override
